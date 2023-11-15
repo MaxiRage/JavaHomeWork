165c1408e2f3765373bc2c89ff82bd2561a824f7
MaxiRage <iarostiuk@ya.ru>
2023-11-15 16:23:05 +0300

    done

---
+++ src/main/java/Employee.java
@@ -0,0 +1,12 @@
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+public class Employee {
+
+    private String name;
+    private long table;
+    private String phone;
+    private int experience;
+
+}
