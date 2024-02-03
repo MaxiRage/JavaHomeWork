143aa16760f098c21e616f6bc173e6e4f141165d
MaxiRage <iarostiuk@ya.ru>
2024-02-03 17:27:10 +0300

    HW done

--- src/main/java/com/example/demo/entity/Person.java
+++ src/main/java/com/example/demo/entity/Person.java
@@ -10,7 +10,7 @@ import lombok.*;
 @Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
